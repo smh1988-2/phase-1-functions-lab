c74ad3653a2f59baa696afb8145bf6554804e114
smh1988-2 <seamhurley1988@gmail.com>
2021-11-02 10:53:18 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(customerLocation) {
+    if (customerLocation > 42) {
+        return customerLocation - 42
+    } else {
+        return 42 - customerLocation
+    }
+}
+
+distanceFromHqInBlocks(43);
+distanceFromHqInBlocks(50);
+distanceFromHqInBlocks(34);
+
+function distanceFromHqInFeet(customerLocation) {
+    return distanceFromHqInBlocks(customerLocation) * 264
+}
+
+distanceFromHqInFeet(43);
+distanceFromHqInFeet(50);
+distanceFromHqInFeet(34);
+
+function distanceTravelledInFeet (startBlock, endBlock) {
+    if (startBlock > endBlock) {
+        return (startBlock - endBlock) * 264
+    } else {
+        return (endBlock - startBlock) * 264
+    }
+};
+
+distanceTravelledInFeet(43, 48);
+distanceTravelledInFeet(50, 60);
+distanceTravelledInFeet(34, 28);
+
+
+function calculatesFarePrice(startBlock, endBlock) {
+    if (distanceTravelledInFeet(startBlock,endBlock) < 400) {
+        return 0
+    } 
+    
+    else if (distanceTravelledInFeet(startBlock,endBlock) > 400 && distanceTravelledInFeet(startBlock,endBlock) < 1999) {
+        return (distanceTravelledInFeet(startBlock,endBlock) - 400) * .02
+    } 
+    
+    else if (distanceTravelledInFeet(startBlock,endBlock) > 2000 && distanceTravelledInFeet(startBlock,endBlock) < 2500) {
+        return 25
+    } 
+    
+    else if (distanceTravelledInFeet(startBlock,endBlock) > 2500) {
+        return "cannot travel that far"
+    }
+}
+
+calculatesFarePrice(43, 48);
+calculatesFarePrice(50, 60);
+calculatesFarePrice(34, 28);
